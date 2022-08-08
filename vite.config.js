206ac16d6d888292c0defd1ff493bbb9de12fe8a
hwn <winning.h@qq.com>
2022-08-08 22:07:21 +0800

    修改环境变量

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,33 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
+import { loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          // 自动引入ant-design/icons-vue中的图标，需要安装@ant-design/icons-vue
-          resolveIcons: true
-        })
-      ]
-    })
-  ],
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            // 自动引入ant-design/icons-vue中的图标，需要安装@ant-design/icons-vue
+            resolveIcons: true
+          })
+        ]
+      })
+    ],
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+    base: loadEnv(mode, process.cwd()).VITE_BASE_ROUTER,
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+
+    server: {
+      port: '8000'
     }
-  },
-
-  server: {
-    port: '8000'
-  }
-})
+  })
+}
